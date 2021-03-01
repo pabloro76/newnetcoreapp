66c2895d016c86b94c34ac3b6f99ef72a6b50175
Pablo Rojas <jurojas@microsoft.com>
2021-03-01 15:35:47 -0700

    testing

---
+++ netcoreStart.ps1
@@ -0,0 +1,5 @@
+﻿dotnet new webapi -o NewAspNetCoreApp --no-https
+#http://localhost:5000/weatherforecast
+docker build -t imgnewnetcoreapp:v1 .
+docker run -it --rm -p 8080:80 imgnewnetcoreapp:v1
+#http://localhost:8080/weatherforecast
