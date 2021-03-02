38097cf25428ec0425a908e23b07235dd6a1f4d9
Pablo Rojas <jurojas@microsoft.com>
2021-03-01 17:10:49 -0700

    Add K8s support

--- netcoreStart.ps1
+++ netcoreStart.ps1
@@ -3,3 +3,15 @@
 docker build -t imgnewnetcoreapp:v1 .
 docker run -it --rm -p 8080:80 imgnewnetcoreapp:v1
 #http://localhost:8080/weatherforecast
+
+#K8s commands
+kubectl config get-contexts
+kubectl apply -f .\deployment.yml
+kubectl get deployments
+kubectl get pods
+kubectl logs newnetcoreapp-8c79b699d-rb249
+#services and WHY!!
+kubectl apply -f .\service.yml
+kubectl get services
+
+kubectl delete svc newnetcore-service
